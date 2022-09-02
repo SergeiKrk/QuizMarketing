232b12b261a80a95c051853f5e5eb5de73bc96ef
ksv <krjukov_s@mail.ru>
2022-09-02 16:19:12 +0400

    refactoring ResponsiveAppBar

--- src/components/firstScreen/ResponsiveAppBar.js
+++ src/components/firstScreen/ResponsiveAppBar.js
@@ -11,7 +11,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+// import AdbIcon from "@mui/icons-material/Adb";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import logo from "../../img/logo-150-50.jpg";
 
@@ -50,23 +50,19 @@ const ResponsiveAppBar = () => {
       <AppBar position="static">
         <Container maxWidth="xl">
           <Toolbar disableGutters>
-            <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+            {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
             <Typography
-              variant="h6"
+              variant="h5"
               noWrap
               component="a"
-              href="/"
+              href=""
               sx={{
-                mr: 2,
+                mr: 1,
                 display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
+                flexGrow: 1,
               }}
             >
-              LOGO
+              <img src={logo} alt="" />
             </Typography>
 
             <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -106,7 +102,7 @@ const ResponsiveAppBar = () => {
               </Menu>
             </Box>
             {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
-            <img src={logo} />
+
             <Typography
               variant="h5"
               noWrap
@@ -116,15 +112,11 @@ const ResponsiveAppBar = () => {
                 mr: 2,
                 display: { xs: "flex", md: "none" },
                 flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
               }}
             >
-              {/* LOGO */}
+              <img src={logo} alt="" />
             </Typography>
+
             <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
               {pages.map((page) => (
                 <Button
